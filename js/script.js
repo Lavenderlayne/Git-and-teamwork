8ec66fc50d086db816a29274a64808c79567c235
volodymyrlogika <volodymyr.logika@gmail.com>
2024-10-29 19:02:34 +0200

    fixed

--- js/script.js
+++ js/script.js
@@ -196,11 +196,45 @@ homeButton.addEventListener('click', () => {
     restartButton.style.display = 'none'
 });
 
+restartButton.addEventListener('click', () => {
+    statisticsDisplay.style.display = 'none'
+    homeButton.style.display = 'none'
+    restartButton.style.display = 'none'
+    questionScreen.style.display = 'block';
+    progressBar.innerHTML = '';
+    startQuiz();
+});
 
-answerButtons.forEach(button => {
-    button.addEventListener('click', function () {
-        const currentQuestion = selectedQuiz[currentQuestionIndex - 1];
-        if (button.innerHTML.trim() === currentQuestion.correct.trim()) {
+
+let progressBar = document.querySelector('.progress-bar');
+
+function updateProgress(isCorrect) {
+    let segment = document.createElement('div');
+    segment.classList.add('progress-segment');
+    segment.style.width = `${100 / selectedQuiz.length}%`;
+
+    if (isCorrect) {
+        segment.classList.add('correct');
+    } else {
+        segment.classList.add('incorrect');
+    }
+
+
+    progressBar.appendChild(segment);
+}
+
+
+// Existing code for answer buttons
+answerButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        let selectedAnswer = button.innerHTML.trim();
+        let correctAnswer = selectedQuiz[currentQuestionIndex - 1].correct.trim();
+        
+        console.log(selectedAnswer);
+        console.log(correctAnswer);
+        
+        if (selectedAnswer == correctAnswer) {
+            button.style.background = '#4CAF50';
             correctAnswersCount++;
             updateProgress(true);
             console.log("Правильно");
